1a16f16c20bc69f8a5d6a092235e84d575b58369
Jeff Khuu <jeffk4@educbe.ca>
2024-10-25 18:11:34 +0000

    Initial Commit

---
+++ code/J12021.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class J12021 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int B = sc.nextInt();
+    int P = 5 * B - 400;
+
+    System.out.println(P);
+    System.out.println(Integer.compare(100, P)); 
+  }
+}
